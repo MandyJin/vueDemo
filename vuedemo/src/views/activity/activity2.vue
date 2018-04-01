da429e87cf5b1db5083daa52a03c1a27f542ac09
765493907@qq.com <765493907@qq.com>
2018-04-01 11:16:24 +0800

    first commit

---
+++ vuedemo/src/views/activity/activity2.vue
@@ -0,0 +1,28 @@
+<style lang='less' rel='stylesheet/less'>
+  .activity{
+    width: 100%;
+    height: 100%;
+  }
+</style>
+<template>
+  <div class="activity">
+   抽奖活动2
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'activity',
+    data () {
+      return {
+
+      }
+    },
+    mounted(){
+
+    },
+    methods:{
+
+    }
+  }
+</script>
